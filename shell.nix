44f8ad9293e10887ceaf973fcf18a49bad149a2f
sinavir <sinavir@sinavir.fr>
2025-04-11 14:38:13 +0200

    feat: init

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ nixpkgs ? (import ./npins).nixpkgs, pkgs ? import nixpkgs { config.allowUnfree = true; } }:
+pkgs.mkShell {
+  nativeBuildInputs = [
+    (pkgs.python3.withPackages (ps: [
+      ps.numpy
+      ps.scipy
+      ps.ipython
+      ps.tqdm
+      ps.h5py
+      ps.ipympl
+      ps.numba
+      ps.matplotlib
+      ps.jupyter
+      ps.jupyterlab-git
+
+      ps.mkdocs
+      ps.mkdocs-material
+      ps.mkdocstrings
+      ps.mkdocstrings-python
+    ]))
+
+    pkgs.vale
+  ];
+}
