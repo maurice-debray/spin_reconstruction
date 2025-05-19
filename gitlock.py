cdcfa44e72cf25455d3cd000f1f99f3165542dbc
sinavir <sinavir@sinavir.fr>
2025-05-19 15:06:00 +0200

    feat(gitlock): add by pass mechanism

--- gitlock.py
+++ gitlock.py
@@ -20,7 +20,9 @@ def get_commit_hash(path=None):
     if path is None:
         path = get_cwd()
     repo = Repo(path, search_parent_directories=True)
-    if repo.is_dirty(untracked_files=True):
+    if os.environ.get("UNSAFE_GIT_DIRTY") != "yes" and repo.is_dirty(
+        untracked_files=True
+    ):
         raise RuntimeError(
             "The git repo is dirty. You must commit everything before running this software"
         )
